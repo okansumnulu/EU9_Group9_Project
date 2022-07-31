2b78ffe63fc2e16fa95da44cb918640e33b436fa
BayramBilal <bilalbayramb22@gmail.com>
2022-07-31 21:51:33 +0300

    first file done

---
+++ src/feature_Metka/Test2_bilal.java
@@ -0,0 +1,10 @@
+package feature_Metka;
+
+public class Test2_bilal {
+    public static void main(String[] args) {
+        String group = "Hello";
+        System.out.println(group);
+    }
+
+
+}
